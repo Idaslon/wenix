321684902b3ee644b508961ff182be2f1e2bd032
Idaslon <garcia.idaslon@gmail.com>
2022-11-24 14:59:22 -0300

    refactor: next structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import { GetStaticProps } from "next";
+
+export const getStaticProps: GetStaticProps = () => {
+  return {
+    redirect: {
+      destination: "/login",
+      permanent: true,
+    },
+  };
+};
+
+const Home = () => {
+  return <h1>Home</h1>;
+};
+
+export default Home;
