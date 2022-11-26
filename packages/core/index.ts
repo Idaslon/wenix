1beafca8a458785cc408066d538113bd7dd40d7a
Idaslon <garcia.idaslon@gmail.com>
2022-11-26 14:01:43 -0300

    refactor: core component export

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -2,5 +2,4 @@ export * from './button';
 export * from './form';
 export * from './input';
 export * from './link';
-export { Typography } from './typography';
-export type { TypographySizes, TypographyVariants } from './typography';
+export * from './typography';
