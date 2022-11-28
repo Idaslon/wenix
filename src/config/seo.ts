e1d4c4942234ad4b783470b73d5a4398ccd1ea04
Idaslon <garcia.idaslon@gmail.com>
2022-11-28 13:48:43 -0300

    feat: env variables

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -1,7 +1,7 @@
 export const defaultSEO = {
   lang: 'en',
-  title: `Wenix`,
-  domain: `wenix.com`,
+  title: 'Wenix',
+  domain: process.env.NEXT_PUBLIC_SITE_DOMAIN as string,
   description: 'Crossweb platform to connect people',
   image: 'https://idaslon-portfolio.s3.eu-central-1.amazonaws.com/Image.png',
 };
