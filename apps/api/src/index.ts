e38f89a49e332c5e3460657e079fa2480b25633d
Idaslon <garcia.idaslon@gmail.com>
2022-12-07 23:53:31 -0300

    feat(api): setup prisma

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Heya!!');
+import prisma from "./prisma";
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Darlon',
+      email: 'darlon@example.com'
+    }
+  })
+
+  console.log(user);
+}
+
+main()
