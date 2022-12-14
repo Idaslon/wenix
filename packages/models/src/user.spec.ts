837e8e9f83d6ff5a4b23a9a5c8270dfa64d73bac
Idaslon <garcia.idaslon@gmail.com>
2022-12-13 22:57:49 -0300

    feat: improve user tests

--- packages/models/src/user.spec.ts
+++ packages/models/src/user.spec.ts
@@ -25,11 +25,31 @@ test('cannot create an user without name', () => {
   ).toThrowError()
 })
 
+test('cannot create an user with empty name', () => {
+  expect(() =>
+    createUser({
+      name: '',
+      email: 'john@example.com',
+      password: '123456',
+    })
+  ).toThrowError()
+})
+
 test('cannot create an user without email', () => {
   expect(() =>
     createUser({
       name: 'John',
       email: undefined as any,
+      password: '123456',
+    })
+  ).toThrowError()
+})
+
+test('cannot create with invalid email', () => {
+  expect(() =>
+    createUser({
+      name: 'John',
+      email: 'noEmail',
       password: '123456',
     })
   ).toThrowError()
@@ -41,6 +61,16 @@ test('cannot create an user without password', () => {
       name: 'John',
       email: 'john@example.com',
       password: undefined as any,
+    })
+  ).toThrowError()
+})
+
+test('cannot create an user with password length less than 6', () => {
+  expect(() =>
+    createUser({
+      name: 'John',
+      email: 'john@example.com',
+      password: '12345',
     })
   ).toThrowError()
 })
