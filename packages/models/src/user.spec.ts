7995b020dcc791c3ef286e0cbce799ebe2bf56ea
Idaslon <garcia.idaslon@gmail.com>
2022-12-16 21:47:35 -0300

    feat: improve user validations and tests

--- packages/models/src/user.spec.ts
+++ packages/models/src/user.spec.ts
@@ -1,76 +1,194 @@
-import { expect, test } from 'vitest'
-import { createUser } from './user'
+import { describe, expect, it, test } from 'vitest'
+import { validateCreateUser, validateUser } from './user'
 
-test('create an user', () => {
-  expect(
-    createUser({
+describe('validate user creation', () => {
+  it('should valid an user creation', () => {
+    expect(
+      validateCreateUser({
+        name: 'John',
+        email: 'john@example.com',
+        password: '123456',
+      })
+    ).toStrictEqual({
       name: 'John',
       email: 'john@example.com',
       password: '123456',
     })
-  ).toStrictEqual({
-    name: 'John',
-    email: 'john@example.com',
-    password: '123456',
+  })
+
+  it('should not valid an user creation without name', () => {
+    expect(() =>
+      validateCreateUser({
+        name: undefined as any,
+        email: 'john@example.com',
+        password: '123456',
+      })
+    ).toThrowError()
+  })
+
+  it('should not valid an user creation with empty name', () => {
+    expect(() =>
+      validateCreateUser({
+        name: '',
+        email: 'john@example.com',
+        password: '123456',
+      })
+    ).toThrowError()
+  })
+
+  it('should not valid an user creation without email', () => {
+    expect(() =>
+      validateCreateUser({
+        name: 'John',
+        email: undefined as any,
+        password: '123456',
+      })
+    ).toThrowError()
+  })
+
+  it('should not valid an user creation with empty email', () => {
+    expect(() =>
+      validateCreateUser({
+        name: 'John',
+        email: '',
+        password: '123456',
+      })
+    ).toThrowError()
+  })
+
+  it('should not valid an user creation with invalid email', () => {
+    expect(() =>
+      validateCreateUser({
+        name: 'John',
+        email: 'wrongEmail',
+        password: '123456',
+      })
+    ).toThrowError()
+  })
+
+  it('should not valid an user creation without password', () => {
+    expect(() =>
+      validateCreateUser({
+        name: 'John',
+        email: 'john@example.com',
+        password: undefined as any,
+      })
+    ).toThrowError()
+  })
+
+  it('should not valid an user creation with password length less than 6', () => {
+    expect(() =>
+      validateCreateUser({
+        name: 'John',
+        email: 'john@example.com',
+        password: '12345',
+      })
+    ).toThrowError()
   })
 })
 
-test('cannot create an user without name', () => {
-  expect(() =>
-    createUser({
-      name: undefined as any,
+describe('validate user', () => {
+  it('should valid an user', () => {
+    expect(
+      validateUser({
+        id: 1,
+        name: 'John',
+        email: 'john@example.com',
+        password: '123456',
+      })
+    ).toStrictEqual({
+      id: 1,
+      name: 'John',
       email: 'john@example.com',
       password: '123456',
     })
-  ).toThrowError()
-})
+  })
 
-test('cannot create an user with empty name', () => {
-  expect(() =>
-    createUser({
-      name: '',
-      email: 'john@example.com',
-      password: '123456',
-    })
-  ).toThrowError()
-})
+  it('should not valid an user without id', () => {
+    expect(() =>
+      validateUser({
+        id: undefined as any,
+        name: 'John',
+        email: 'john@example.com',
+        password: '123456',
+      })
+    ).toThrowError()
+  })
 
-test('cannot create an user without email', () => {
-  expect(() =>
-    createUser({
-      name: 'John',
-      email: undefined as any,
-      password: '123456',
-    })
-  ).toThrowError()
-})
+  it('should not valid an user without name', () => {
+    expect(() =>
+      validateUser({
+        id: 1,
+        name: undefined as any,
+        email: 'john@example.com',
+        password: '123456',
+      })
+    ).toThrowError()
+  })
 
-test('cannot create with invalid email', () => {
-  expect(() =>
-    createUser({
-      name: 'John',
-      email: 'noEmail',
-      password: '123456',
-    })
-  ).toThrowError()
-})
+  it('should not valid an user with empty name', () => {
+    expect(() =>
+      validateUser({
+        id: 1,
+        name: '',
+        email: 'john@example.com',
+        password: '123456',
+      })
+    ).toThrowError()
+  })
 
-test('cannot create an user without password', () => {
-  expect(() =>
-    createUser({
-      name: 'John',
-      email: 'john@example.com',
-      password: undefined as any,
-    })
-  ).toThrowError()
-})
+  it('should not valid an user without email', () => {
+    expect(() =>
+      validateUser({
+        id: 1,
+        name: 'John',
+        email: undefined as any,
+        password: '123456',
+      })
+    ).toThrowError()
+  })
 
-test('cannot create an user with password length less than 6', () => {
-  expect(() =>
-    createUser({
-      name: 'John',
-      email: 'john@example.com',
-      password: '12345',
-    })
-  ).toThrowError()
+  it('should not valid an user with empty email', () => {
+    expect(() =>
+      validateUser({
+        id: 1,
+        name: 'John',
+        email: '',
+        password: '123456',
+      })
+    ).toThrowError()
+  })
+
+  it('should not valid an user with invalid email', () => {
+    expect(() =>
+      validateUser({
+        id: 1,
+        name: 'John',
+        email: 'wrongEmail',
+        password: '123456',
+      })
+    ).toThrowError()
+  })
+
+  it('should not valid an user without password', () => {
+    expect(() =>
+      validateUser({
+        id: 1,
+        name: 'John',
+        email: 'john@example.com',
+        password: undefined as any,
+      })
+    ).toThrowError()
+  })
+
+  it('should not valid an user with password length less than 6', () => {
+    expect(() =>
+      validateUser({
+        id: 1,
+        name: 'John',
+        email: 'john@example.com',
+        password: '12345',
+      })
+    ).toThrowError()
+  })
 })
--- packages/models/src/user.ts
+++ packages/models/src/user.ts
@@ -1,22 +1,30 @@
 import { z } from 'zod'
 
-const userSchema = z.object({
-  name: z.string(),
+const baseUserSchema = z.object({
+  name: z.string().min(1),
   email: z.string().email(),
   password: z.string().min(6),
 })
 
-export type CreateUserProps = z.infer<typeof userSchema>
+const createUserSchema = baseUserSchema
+
+const userSchema = baseUserSchema.merge(
+  z.object({
+    id: z.number(),
+  })
+)
+
+export type CreateUserProps = z.infer<typeof createUserSchema>
 export type ValidateUserProps = z.infer<typeof userSchema>
 
 export type User = z.infer<typeof userSchema>
 
-export function validateUser(props: ValidateUserProps) {
-  userSchema.parse(props)
-  return true
+export function validateCreateUser(props: CreateUserProps) {
+  const user = createUserSchema.parse(props)
+  return user
 }
 
-export function createUser(props: CreateUserProps) {
+export function validateUser(props: ValidateUserProps) {
   const user = userSchema.parse(props)
   return user
 }
