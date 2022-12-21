e042a2290a3cbaf41f1b1435db0cc46954339c4f
Idaslon <garcia.idaslon@gmail.com>
2022-12-21 15:46:32 -0300

    chore: update dependecies

--- apps/api/src/services/implementation/jwt-auth-service.ts
+++ apps/api/src/services/implementation/jwt-auth-service.ts
@@ -1,16 +1,16 @@
-import { sign, verify } from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import { authConfig } from '../../config/auth'
 import { AuthPayload, AuthService } from '../auth-service'
 
 export class JWTAuthService implements AuthService {
   generateToken(payload: AuthPayload): string {
-    const token = sign(payload, authConfig.secret, { expiresIn: authConfig.expiresIn })
+    const token = jwt.sign(payload, authConfig.secret, { expiresIn: authConfig.expiresIn })
 
     return token
   }
 
   validateToken(token: string): AuthPayload {
-    const tokenDecoded = verify(token, authConfig.secret)
+    const tokenDecoded = jwt.verify(token, authConfig.secret)
 
     if (typeof tokenDecoded === 'string' || !tokenDecoded.email) {
       throw new Error('Invalid token')
