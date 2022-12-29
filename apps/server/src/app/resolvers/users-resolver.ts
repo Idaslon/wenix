913ee50da8c3909b7cf1b22d52be6d25b39c694c
Idaslon <garcia.idaslon@gmail.com>
2022-12-29 09:43:28 -0300

    refactor: remove unused resolver function

--- apps/server/src/app/resolvers/users-resolver.ts
+++ apps/server/src/app/resolvers/users-resolver.ts
@@ -1,6 +1,5 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql'
+import { Query, Resolver } from 'type-graphql'
 import prisma from '../../prisma'
-import { CreateUserInput } from '../dtos/inputs/create-user-input'
 import { UserModel } from '../dtos/models/user-model'
 
 @Resolver(() => UserModel)
@@ -10,18 +9,4 @@ export class UsersResolver {
     const users = await prisma.user.findMany()
     return users
   }
-
-  // @Mutation(() => UserModel)
-  // async createUser(@Arg('data') data: CreateUserInput) {
-  //   const { name, email, password } = data
-  //   const createUser = new CreateUser()
-
-  //   const user = await createUser.execute({
-  //     name,
-  //     email,
-  //     password,
-  //   })
-
-  //   return user
-  // }
 }
