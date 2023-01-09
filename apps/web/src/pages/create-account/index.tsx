2ed4604969675fee1cb6823b00c558cb9d819530
Idaslon <garcia.idaslon@gmail.com>
2023-01-08 23:10:59 -0300

    feat(web): create account form validation

--- apps/web/src/pages/create-account/index.tsx
+++ apps/web/src/pages/create-account/index.tsx
@@ -4,6 +4,8 @@ import { SEO } from '../../components/seo'
 import { RegisterInput, useRegisterMutation } from '../../graphql'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
+import { safeFormValidation } from '../../utils/form'
+import { createUserSchema } from '@wenix/validations'
 
 const Container = styled('div', {
   display: 'flex',
@@ -26,12 +28,20 @@ const _CreateAccount = () => {
 
   const [formErrors, setFormErrors] = useState<CreateAccountFormErrors>()
 
-  const handleCreateAccount = async (data: RegisterInput) => {
+  const handleCreateAccount = async (formData: RegisterInput) => {
+    const { data, issues } = safeFormValidation(createUserSchema, formData)
+
+    if (issues) {
+      setFormErrors(issues)
+      return
+    }
+
     await mutateAsync({ data }).catch((error: Error) => {
       const errorsMessages = messagesMap[error.message]
       setFormErrors(errorsMessages)
     })
 
+    setFormErrors(undefined)
     router.push('/login')
   }
 
