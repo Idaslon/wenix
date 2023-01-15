f47d610f5fb1f00df5b338eab1bfc4bd67f573e9
Idaslon <garcia.idaslon@gmail.com>
2023-01-14 22:42:54 -0300

    refactor(web): link

--- apps/web/src/components/link.tsx
+++ apps/web/src/components/link.tsx
@@ -1,7 +1,11 @@
+import { LinkProps as NextLinkProps } from 'next/link'
 import { default as NextLink } from 'next/link'
 import { styled } from '@wenix/ui'
 
-export const StyledLink = styled(NextLink, {
+export type LinkProps = React.PropsWithChildren<NextLinkProps>
+export type LinkRef = NextLinkProps
+
+export const Link = styled(NextLink, {
   color: '$common-white',
   textDecoration: 'none',
 
--- apps/web/src/components/link/index.tsx
+++
@@ -1,7 +0,0 @@
-import { LinkProps as NextLinkProps } from 'next/link'
-import { StyledLink } from './styles'
-
-export type LinkProps = React.PropsWithChildren<NextLinkProps>
-export type LinkRef = NextLinkProps
-
-export const Link = StyledLink
